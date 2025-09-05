c3f8641b9f3b4af12964d318c64b71ffa1b9e61e
OziinG <dlajiin@gmail.com>
2025-09-05 13:42:23 +0900

    feat: domain setting

---
+++ ContentView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ContentView has been removed.")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WorkoutLogApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct WorkoutLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
