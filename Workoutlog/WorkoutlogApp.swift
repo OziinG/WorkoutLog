91d8e91e93a2f18e0b366c5191c1311714105a91
OziinG <dlajiin@gmail.com>
2025-09-08 14:13:35 +0900

    Initial Commit

---
+++ Workoutlog/WorkoutlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutlogApp.swift
+//  Workoutlog
+//
+//  Created by Lim jiin on 9/8/25.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WorkoutlogTests/WorkoutlogTests.swift
@@ -0,0 +1,16 @@
+//
+//  WorkoutlogTests.swift
+//  WorkoutlogTests
+//
+//  Created by Lim jiin on 9/8/25.
+//
+
+import Testing
+
+struct WorkoutlogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
